213151946324991affe76a2085d3820f27adb291
FXC-ai <fx.coindreau@gmail.com>
2021-09-15 12:42:06 +0200

    Initialisation des données initiales

---
+++ Testat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 12:36:01 2021
+
+@author: oem
+"""
+
+sensibilite = 0.8
+specificite = 0.9
+
+population = 1000
+p_malades = 0.05
+
